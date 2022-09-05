64ebebcad672a170d9801bb1149810aeae2d3c43
Neil Betham <nbetham@me.com>
2022-09-04 23:54:40 -0400

    Working low level I2C transactions

--- src/i2c.cpp
+++ src/i2c.cpp
@@ -46,27 +46,34 @@ void I2C::init() {
   }
 
   // Setup the prescaler and hold times
+  // Defaulting to fast mode 400khz
   switch(_i2c_number) {
   case 1:
     // Assumes a 16 mhz clock source
-    I2C_1_TIMINGR &= (BIT_27 | BIT_26 | BIT_25 | BIT_24);
-    I2C_1_TIMINGR |= 0x4;  // SCLL
-    I2C_1_TIMINGR |= 0x2 << 8; // SCLH
-    I2C_1_TIMINGR |= 0x2 << 20l; // SCLDEL
+    I2C_1_TIMINGR = 0;
+    I2C_1_TIMINGR |= 0x9;  // SCLL
+    I2C_1_TIMINGR |= 0x3 << 8; // SCLH
+    I2C_1_TIMINGR |= 0x2 << 16; // SDADEL
+    I2C_1_TIMINGR |= 0x3 << 20; // SCLDEL
+    I2C_1_TIMINGR |= 0x1 << 28; // PRESC
     break;
   case 2:
     // Assumes a 16 mhz clock source
-    I2C_2_TIMINGR &= (BIT_27 | BIT_26 | BIT_25 | BIT_24);
-    I2C_2_TIMINGR |= 0x4;  // SCLL
-    I2C_2_TIMINGR |= 0x2 << 8; // SCLH
-    I2C_2_TIMINGR |= 0x2 << 20l; // SCLDEL
+    I2C_2_TIMINGR = 0;
+    I2C_2_TIMINGR |= 0x9;  // SCLL
+    I2C_2_TIMINGR |= 0x3 << 8; // SCLH
+    I2C_2_TIMINGR |= 0x2 << 16; // SDADEL
+    I2C_2_TIMINGR |= 0x3 << 20; // SCLDEL
+    I2C_2_TIMINGR |= 0x1 << 28; // PRESC
     break;
   case 3:
     // Assumes a 16 mhz clock source
-    I2C_3_TIMINGR &= (BIT_27 | BIT_26 | BIT_25 | BIT_24);
-    I2C_3_TIMINGR |= 0x4;  // SCLL
-    I2C_3_TIMINGR |= 0x2 << 8; // SCLH
-    I2C_3_TIMINGR |= 0x2 << 20l; // SCLDEL
+    I2C_3_TIMINGR = 0;
+    I2C_3_TIMINGR |= 0x9;  // SCLL
+    I2C_3_TIMINGR |= 0x3 << 8; // SCLH
+    I2C_3_TIMINGR |= 0x2 << 16; // SDADEL
+    I2C_3_TIMINGR |= 0x3 << 20; // SCLDEL
+    I2C_3_TIMINGR |= 0x1 << 28; // PRESC
     break;
   }
 
@@ -86,23 +93,101 @@ void I2C::init() {
   // Enable the peripheral
   switch(_i2c_number) {
   case 1:
-    I2C_1_CR1 &= ~(BIT_0);
+    I2C_1_CR1 |= BIT_0;
     break;
   case 2:
-    I2C_2_CR1 &= ~(BIT_0);
+    I2C_2_CR1 |= BIT_0;
     break;
   case 3:
-    I2C_3_CR1 &= ~(BIT_0);
+    I2C_3_CR1 |= BIT_0;
     break;
   }
 }
 
 int I2C::write_to(uint8_t addr, uint8_t reg, const uint8_t* data, uint32_t len) {
-  return 0;
+  // Set slave address
+  REGISTER(_base_addr + I2C_CR2_OFFSET) &= ~(0x000003FF);
+  REGISTER(_base_addr + I2C_CR2_OFFSET) |= (addr << 1);
+
+  // Set transfer dir
+  REGISTER(_base_addr + I2C_CR2_OFFSET) &= ~BIT_10;
+
+  // Set number of bytes to be transferred
+  REGISTER(_base_addr + I2C_CR2_OFFSET) &= ~(0xFF << 16);
+  REGISTER(_base_addr + I2C_CR2_OFFSET) |= (((len + 1) & 0xFF) << 16);
+
+  // Unset auto end mode
+  REGISTER(_base_addr + I2C_CR2_OFFSET) &= ~BIT_25;
+
+  // Start the transaction
+  REGISTER(_base_addr + I2C_CR2_OFFSET) |= BIT_13;
+
+  // Start writing bytes
+  // Register addr first
+  while(!(REGISTER(_base_addr + I2C_ISR_OFFSET) & BIT_1));
+  REGISTER(_base_addr + I2C_TXDR_OFFSET) = reg;
+
+  // Then data
+  for(uint32_t index = 0; index < len; index++) {
+    while(!(REGISTER(_base_addr + I2C_ISR_OFFSET) & BIT_1));
+    REGISTER(_base_addr + I2C_TXDR_OFFSET) = data[index];
+  }
+
+  // Wait for transmission complete
+  while(!(REGISTER(_base_addr + I2C_ISR_OFFSET) & BIT_6));
+
+  // Send stop
+  REGISTER(_base_addr + I2C_CR2_OFFSET)  |= BIT_14;
+
+  return len;
 }
 
 int I2C::read_from(uint8_t addr, uint8_t reg, uint8_t* data, uint32_t len) {
-  return 0;
+  // First write the register to the target device
+  // Set slave address
+  REGISTER(_base_addr + I2C_CR2_OFFSET) &= ~(0x000003FF);
+  REGISTER(_base_addr + I2C_CR2_OFFSET) |= (addr << 1);
+
+  // Set transfer dir
+  REGISTER(_base_addr + I2C_CR2_OFFSET) &= ~BIT_10;
+
+  // Set number of bytes to be transferred
+  REGISTER(_base_addr + I2C_CR2_OFFSET) &= ~(0x000000FF << 16);
+  REGISTER(_base_addr + I2C_CR2_OFFSET) |= (0x1 << 16);
+
+  // Unset auto end mode
+  REGISTER(_base_addr + I2C_CR2_OFFSET) &= ~BIT_25;
+
+  // Start the transaction
+  REGISTER(_base_addr + I2C_CR2_OFFSET) |= BIT_13;
+
+  // Write register addr
+  while(!(REGISTER(_base_addr + I2C_ISR_OFFSET) & BIT_1));
+  REGISTER(_base_addr + I2C_TXDR_OFFSET) = reg;
+
+  // Wait for the transaction to complete
+  while(!(REGISTER(_base_addr + I2C_ISR_OFFSET) & BIT_6));
+
+  // Set the number of bytes to be received
+  REGISTER(_base_addr + I2C_CR2_OFFSET) &= ~(0x000000FF << 16);
+  REGISTER(_base_addr + I2C_CR2_OFFSET) |= ((len & 0xFF) << 16);
+
+  // Set transfer dir
+  REGISTER(_base_addr + I2C_CR2_OFFSET) |= BIT_10;
+
+  // Start the rx end of the transaction
+  REGISTER(_base_addr + I2C_CR2_OFFSET)  |= BIT_13;
+
+  // Wait for the data to come in
+  for(uint32_t index = 0; index < len; index++) {
+    while(!(REGISTER(_base_addr + I2C_ISR_OFFSET) & BIT_2));
+    data[index] = REGISTER(_base_addr + I2C_RXDR_OFFSET);
+  }
+
+  // Send stop
+  REGISTER(_base_addr + I2C_CR2_OFFSET)  |= BIT_14;
+
+  return len;
 }
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include "registers/gpio.h"
 #include "registers/flash.h"
 
+#include "i2c.h"
+
 int main() {
   // Up main clock frequency
   // Turn off PLL and wait for it be ready
@@ -29,15 +31,53 @@ int main() {
   // Switch system clock to PLL
   RCC_CFGR |= (0x3 << 0);
 
-  // Setup GPIO for blinken lights
+  // Setup the I2C peripheral
+  I2C ptn5110(1);
+  ptn5110.init();
+
+  // Enable IO Port A
   RCC_IOPENR |= 0x0000009F;
-  GPIO_A_MODER  = 0xEB555505;
+
+  // Setup GPIO A 9/10 for I2C 1
+  GPIO_A_AFRH    &= ~(0x000000FF << 4);
+  GPIO_A_AFRH     |= (0x00000011 << 4);
+
+  GPIO_A_MODER   &= ~(0x0000000F << 18);
+  GPIO_A_MODER    |= (0x0000000A << 18);
+
+  GPIO_A_OSPEEDR &= ~(0x0000000F << 18);
+  GPIO_A_OSPEEDR  |= (0x0000000F << 18);
+
+  GPIO_A_PUPDR   &= ~(0x0000000F << 18);
+  GPIO_A_PUPDR    |= (0x00000005 << 18);
+
+  GPIO_A_OTYPER   |= (0x00000003 << 9);
+
+  // Check the vendor ID for the PTN5110
+  const uint8_t ptn5110_addr = 0x52;
+  uint8_t buffer[2];
+
+  for(uint32_t index = 0; index < 100000; index++);
+  ptn5110.read_from(ptn5110_addr, 0, buffer, 2);
+
+  uint8_t alert_mask[2] = {0xFF, 0x7F};
+  ptn5110.write_to(ptn5110_addr, 0x12, alert_mask, 2);
+  ptn5110.read_from(ptn5110_addr, 0x12, alert_mask, 2);
+
+
+  // Setup GPIO for blinken lights
+  GPIO_A_MODER  &= ~(0x0000030F);
+  GPIO_A_MODER  |= 0x00000105;
   GPIO_A_OTYPER |= 0x00000013;
   GPIO_A_ODR   = 0x00000013;
 
+  if(buffer[0] == 0xC9 && buffer[1] == 0x1F) {
+    GPIO_A_ODR &= ~(BIT_3 | BIT_1);
+  }
+
   while(1) {
-    GPIO_A_ODR = (~(GPIO_A_ODR & 0x00000013)) & 0x00000013;
-    for(uint32_t i = 0; i < 500000; i++);
+//    GPIO_A_ODR = (~(GPIO_A_ODR & 0x00000013)) & 0x00000013;
+    for(uint32_t i = 0; i < 2000000; i++);
   }
 
   return 0;
